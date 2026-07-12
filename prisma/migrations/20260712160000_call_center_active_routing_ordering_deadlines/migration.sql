0e601512a3ec2363c1e29cb7446d21db439dcf0a
Chase Fagen <fagenchase@gmail.com>
2026-07-12 20:04:25 +0100

    Index active call recovery deadlines

--- prisma/migrations/20260712160000_call_center_active_routing_ordering_deadlines/migration.sql
+++ prisma/migrations/20260712160000_call_center_active_routing_ordering_deadlines/migration.sql
@@ -2,6 +2,9 @@ ALTER TABLE "call_center_call"
 ADD COLUMN "deadlineAt" TIMESTAMP(3),
 ADD COLUMN "queueDeadlineAt" TIMESTAMP(3);
 
+CREATE INDEX "call_center_call_direction_effectOwner_status_deadlineAt_idx"
+ON "call_center_call"("direction", "effectOwner", "status", "deadlineAt");
+
 ALTER TABLE "call_center_command"
 ADD COLUMN "dependsOnCommandId" TEXT;
 
